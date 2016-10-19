c810a03e8a1126b2a1a1fb34a6949c66dfd7bec0
jasonwbeard <jwbeard@gmail.com>
2016-10-19 14:37:08 -0700

    Update profilelist.sh

--- profilelist.sh
+++ profilelist.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+# Author: Jason Beard
+# Purpose: List the verbose names of OSX configuration profiles (i.e. from Casper)
+
 # List the Profile Names with their Profile Identifiers.
 CompLevelProfiles=`profiles -C -v | awk -F: '/attribute: name/{print $NF}''/attribute: profileIdentifier/{print $NF "\n"}' | sed 's/ //'`
 echo "<result>\n$CompLevelProfiles\n</result>"
