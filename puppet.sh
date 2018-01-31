b5e2298d6c1004cd116268903fff654f424556c6
jasonwbeard <jwbeard@gmail.com>
2018-01-31 15:32:10 -0600

    Add files via upload

--- puppet.sh
+++ puppet.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
 
-# define the IP address of the puppetmaster
+# define the IP address and hostname of the puppetmaster
 
 PM_IP=34.214.79.56
+PM_HOSTNAME=linuxmdm
 
 # add the puppetmaster's hostname to DNS
 # this is required for cert trust; can be deprecated when tradeshift.com DNS is updated
 
-if grep -q "$PM_IP linuxmdm.tradeshift.com linuxmdm" /etc/hosts; then
-	echo "the correct puppetmaster (linuxmdm) IP is already in the hosts file, skipping..."
-elif grep -q "linuxmdm.tradeshift.com" /etc/hosts; then
-	echo "adding puppetmaster (linuxmdm) to the hosts file..."
-	sed -i '/linuxmdm.tradeshift.com/d' /etc/hosts
-	sed -i "2i$PM_IP linuxmdm.tradeshift.com linuxmdm" /etc/hosts
+if grep -q "$PM_IP $PM_HOSTNAME.tradeshift.com $PM_HOSTNAME" /etc/hosts; then
+	echo "the correct puppetmaster ($PM_HOSTNAME) IP is already in the hosts file, skipping..."
+elif grep -q "$PM_HOSTNAME.tradeshift.com" /etc/hosts; then
+	echo "adding puppetmaster ($PM_HOSTNAME) to the hosts file..."
+	sed -i "/$PM_HOSTNAME.tradeshift.com/d" /etc/hosts
+	sed -i "2i$PM_IP $PM_HOSTNAME.tradeshift.com $PM_HOSTNAME" /etc/hosts
 else
-	echo "adding puppetmaster (linuxmdm) to the hosts file..."
-	sed -i "2i$PM_IP linuxmdm.tradeshift.com linuxmdm" /etc/hosts
+	echo "adding puppetmaster ($PM_HOSTNAME) to the hosts file..."
+	sed -i "2i$PM_IP $PM_HOSTNAME.tradeshift.com $PM_HOSTNAME" /etc/hosts
 fi
 
 # curl is needed to grab the join script, if it isn't already installed
@@ -28,4 +29,4 @@ fi
 
 # download and run the join script
 
-curl -k https://linuxmdm.tradeshift.com:8140/packages/current/install.bash | bash
+curl -k https://$PM_HOSTNAME.tradeshift.com:8140/packages/current/install.bash | bash
